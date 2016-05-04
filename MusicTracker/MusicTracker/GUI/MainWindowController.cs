50f0bf53e1c24ffbb7fdc90caf7551920ba14d23
Vultour <vultour@users.noreply.github.com>
2016-05-04 02:44:03 +0100

    Add resources

--- MusicTracker/MusicTracker/GUI/MainWindowController.cs
+++ MusicTracker/MusicTracker/GUI/MainWindowController.cs
@@ -3,17 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
+using MusicTracker.Core;
+
 
 namespace MusicTracker.GUI
 {
     class MainWindowController
     {
+        private MusicList musicList;
+
+
         public MainWindow View { get; private set; }
 
 
         public MainWindowController()
         {
+            this.musicList = new MusicList();
             this.View = new MainWindow();
+        }
+
+
+        private void loadPrevious()
+        {
+            if (!Directory.Exists(Properties.Resources.SAVE_FILE_PATH)) { Directory.CreateDirectory(Properties.Resources.SAVE_FILE_PATH); }
+            if (!File.Exists(String.Format("{0}\\{1}", Properties.Resources.SAVE_FILE_PATH, Properties.Resources.SAVE_FILE_NAME)))
+            {
+                this.musicList
+            }
         }
 
 
