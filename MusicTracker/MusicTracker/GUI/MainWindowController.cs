7f72355a57e717b1f12e2bb1a33fb408bb0ebbcd
Vultour <vultour@users.noreply.github.com>
2016-05-04 06:15:45 +0100

    Add basic save and load

--- MusicTracker/MusicTracker/GUI/MainWindowController.cs
+++ MusicTracker/MusicTracker/GUI/MainWindowController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Windows.Forms;
 
 using MusicTracker.Core;
 
@@ -27,14 +28,60 @@ namespace MusicTracker.GUI
 
         private void loadPrevious()
         {
-            if (!Directory.Exists(Properties.Resources.SAVE_FILE_PATH)) { Directory.CreateDirectory(Properties.Resources.SAVE_FILE_PATH); }
+            if (!Directory.Exists(Properties.Resources.SAVE_FILE_PATH))
+            {
+                try { Directory.CreateDirectory(Properties.Resources.SAVE_FILE_PATH); }
+                catch (Exception)
+                {
+                    MessageBox.Show(String.Format("{1}{0}{2}", Environment.NewLine, "Unable to create application directory.", String.Format("Make sure that {0} can be created", Properties.Resources.SAVE_FILE_PATH)), "Fatal error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Application.Exit();
+                }
+            }
             if (!File.Exists(String.Format("{0}\\{1}", Properties.Resources.SAVE_FILE_PATH, Properties.Resources.SAVE_FILE_NAME)))
             {
-                this.musicList
+                try { this.musicList.Save(String.Format("{0}\\{1}", Properties.Resources.SAVE_FILE_PATH, Properties.Resources.SAVE_FILE_NAME)); }
+                catch (Exception)
+                {
+                    MessageBox.Show(String.Format("{1}{0}{2}", Environment.NewLine, "Unable to create a save file!", String.Format("Make sure {0} is writable", Properties.Resources.SAVE_FILE_PATH)), "Fatal error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Application.Exit();
+                }
+            }
+            else
+            {
+                try { this.musicList = MusicList.Load(String.Format("{0}\\{1}", Properties.Resources.SAVE_FILE_PATH, Properties.Resources.SAVE_FILE_NAME)); }
+                catch (Exception)
+                {
+                    MessageBox.Show(String.Format("{1}{0}{2}{0}{3}", Environment.NewLine, "Unable to open the save file!", "The file might be corrupted.", String.Format("Try recovering your data from {0}\\{1} and delete the file when done", Properties.Resources.SAVE_FILE_PATH, Properties.Resources.SAVE_FILE_NAME)), "Fatal error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Application.Exit();
+                }
             }
         }
 
 
         // TODO: Add event handlers
+        public void AddNotDownloadedHandler(object sender, AddTrackEventArgs e)
+        {
+            // TODO
+        }
+
+        public void AddDownloadedHandler(object sender, AddTrackEventArgs e)
+        {
+            // TODO
+        }
+
+        public void SelectTrackHandler(object sender, SelectTrackEventArgs e)
+        {
+            // TODO
+        }
+
+        public void SaveTrackHandler(object sender, SaveTrackEventArgs e)
+        {
+            // TODO
+        }
+
+        public void CleanUnusedGenresHandler(object sender, EventArgs e)
+        {
+            // TODO
+        }
     }
 }
